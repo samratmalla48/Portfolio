4a2f207c9403d795482c92cdafb2902876fb026d
samrat malla <samratmalla48@gmail.com>
2023-11-10 08:24:06 +1100

    header section

--- myportfolio/portfolio/src/App.js
+++ myportfolio/portfolio/src/App.js
@@ -1,8 +1,10 @@
 import Navbar from "./components/Navbar/navbar.js";
+import Intro from './components/Navbar/Intro/intro.js';
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Intro />
     </div>
   );
 }
---
+++ myportfolio/portfolio/src/components/Navbar/Intro/intro.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./intro.css";
+
+import bg from "../../../assets/new.png";
+import { Link } from "react-scroll";
+const Intro = () => {
+  return (
+    <div>
+      <section id="intro">
+        <div className="introContent">
+
+          <span className="hello">Hello</span>
+          <span className="introText">I am</span>
+          <span className="introName">Samrat</span>
+          <p className="introPara">This is the paragraph section</p>
+          <Link>
+            <button className="btn">Hire Me</button>
+          </Link>
+        </div>
+
+        <img src={bg} alt="Profile" className="profPic" />
+      </section>
+    </div>
+  );
+};
+
+export default Intro;
